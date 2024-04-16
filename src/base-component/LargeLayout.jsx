f0b4f9f75151cf34fc1ff230c743e537aad2fefe
Harout Mardirossian <harmardir@users.noreply.github.com>
2024-04-16 10:33:47 +0300

    adding banner image

--- src/base-component/LargeLayout.jsx
+++ src/base-component/LargeLayout.jsx
@@ -8,11 +8,9 @@ import classNames from 'classnames';
 
 const LargeLayout = ({ intl }) => (
 
-  <div 
-      className="w-50 d-flex banner__image large-layout"
-      style={{ backgroundImage: `url('https://undp-lms.kashida-learning.co/static/undp_ac_theme/images/banner_large.png')` }}
- >
-    <div className="col-md-9 bg-primary-400">
+    <div className="col-md-9 bg-primary-400"
+    style={{ backgroundImage: `url('https://undp-lms.kashida-learning.co/static/undp_ac_theme/images/banner_large.png')`}}
+    >
       <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
         <Image className="logo position-absolute" alt={getConfig().SITE_NAME} src={getConfig().LOGO_WHITE_URL} />
       </Hyperlink>
